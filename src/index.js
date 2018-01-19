4316f52a83d8264daa2cc8db558b3a8d4edbd00e
nish <nishanth.vincent1@gmail.com>
2018-01-18 22:35:07 -0800

    added a basic input component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SearchBar from './components/search-bar';
 
+console.log(SearchBar);
 
-const App = function(){
-  return '<div>Hello!</div>'
+const API_KEY = 'AIzaSyC37u4ogaD6lfik8pggXj3JfhO_5tRham0';
+
+const App = () => {
+  return (<div>
+            <SearchBar />
+          </div>);
 }
+
+ReactDOM.render(<App />, document.querySelector('.container'));
