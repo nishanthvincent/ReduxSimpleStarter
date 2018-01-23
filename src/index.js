0ea7dc1231b4a5db3e82b4275815773c35bf7330
Nish <nishanth.fairfield@gmail.com>
2018-01-23 07:06:41 -0800

    Refactor App to be a class component

--- src/index.js
+++ src/index.js
@@ -4,10 +4,6 @@ import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search-bar';
 const API_KEY = 'AIzaSyC37u4ogaD6lfik8pggXj3JfhO_5tRham0';
 
-YTSearch({key: API_KEY, term: 'muzhuval john'}, (data) => {
-  console.log(data);
-});
-
 
 
 class App extends Component {
@@ -15,7 +11,13 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {videos: []};
+    
+    YTSearch({key: API_KEY, term: 'muzhuval john'}, (videos) => {
+      this.setState(videos);
+      console.log(this.state);
+    });
+    
   }
 
   render() {
