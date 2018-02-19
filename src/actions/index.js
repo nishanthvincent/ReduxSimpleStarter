275a24e17b6bb4b963dcf114ceb826c6e643534d
Nish <nishanth.fairfield@gmail.com>
2018-02-18 23:41:53 -0800

    wip

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,7 @@
-export default function selectBook(book) {
-  console.log('Select book called ', book.title);
+export function selectBook(book) {
+  // Select book is an action creator, it needs to return with a type and an optional payload
+  return {
+    type: 'BOOK_SELECTED',
+    payload: book
+  }
 }
---
+++ src/reducers/active-book.js
@@ -0,0 +1,8 @@
+// The state object is not the application state, but the state this reducer is responsible for.
+export default function(state = null, action) {
+  switch(action.type){
+    case: 'BOOK_SELECTED'
+      return action.payload
+  }
+  return state;
+}
